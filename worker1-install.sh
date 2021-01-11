da70484cc653820d01d0031caa2b1234741ad32e
Jessychan98 <72158996+Jessychan98@users.noreply.github.com>
2021-01-11 14:21:19 +0100

    Create worker1-install.sh

---
+++ worker1-install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Update & Upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install Curl
+sudo apt-get install curl -y
+
+# Install, start & enable Docker
+sudo apt-get install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Install kubernetes
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt update && apt install -y kubeadm=1.16.0-00 kubelet=1.16.0-00 kubectl=1.16.0-00
+
+# Kubernetes Deployment
+sudo swapoff –a
+sudo hostnamectl set-hostname worker1
+sudo reboot
